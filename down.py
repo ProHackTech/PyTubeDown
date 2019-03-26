3e4d7a10943d8a8d98c9200ad46339d8bb03df89
ProHack <bhavesh@prohack.tech>
2019-03-27 05:11:17 +0800

    Update
    add colors and error handling
    simplify init
    fix progressbar

--- down.py
+++ down.py
@@ -1,33 +1,35 @@
+import threading, argparse
+from support.colors import *
+from support.errors import *
 from time import sleep
-import threading
-from pytube import YouTube
-import argparse
 from tqdm import tqdm
+from pytube import YouTube
 from selenium import webdriver
 
-
+# replace quotes in string
 def remove_quotes(_string_):
 	_string_ = _string_.replace('"', '')
 	return _string_
 
+# replce blank space in string
 def replace_blank_space(_string_):
 	_string_ = _string_.replace(' ', '+')
 	return _string_
 
+# download vidos
 def download_video(video):
 	if video == "https://www.youtube.com/None":
-		pass
+		print(f"{warning}Video 'None' type! Exiting..")
+		exit()
 	else:
 		try:
 			YouTube(video).streams.first().download()
 		except:
 			pass
-		
 
+# generate video links
 def get_vids(topic, scrl):
-
 	video_links = []
-
 	topic = remove_quotes(topic)
 	topic = replace_blank_space(topic)
 	site = f"https://www.youtube.com/results?search_query={topic}"
@@ -53,17 +55,17 @@ def get_vids(topic, scrl):
 	pbar = tqdm(total=len(rippers))
 	for ripper in rippers:
 		ripper.start()
-	for ripper in rippers:
 		ripper.join()
 		pbar.update(1)
 
-def init():
-	parser = argparse.ArgumentParser(description="PyDown v1.0")
-	parser.add_argument("-t", "--topic", type=str ,help="Enter topic name")
-	parser.add_argument("-scrl", "--scroll", type=int, help="Enter max scroll")
-
-	args = parser.parse_args()
-	get_vids(args.topic, args.scroll)
-
-if __name__ == "__main__":
-	init()
+parser = argparse.ArgumentParser(description="PyDown v1.0")
+parser.add_argument("-t", "--topic", type=str ,help="Enter topic name")
+parser.add_argument("-scrl", "--scroll", type=int, help="Enter max scroll")
+args = parser.parse_args()
+if args.topic:
+	if args.scroll:
+		get_vids(args.topic, args.scroll)
+	else:
+		get_vids(args.topic, 0)
+else:
+	print(f"{error}Please specify a video topic{print_help}")
---
+++ support/errors.py
@@ -0,0 +1,6 @@
+from support.colors import *
+
+error = f'{c_blue}[ERROR] {c_red}'
+warning = f'{c_blue}[!] {c_yellow}'
+success = f'{c_blue}[+] {c_green}'
+print_help = f'\n{c_white}Type: -h/--help for help [Ex: script.py -h]'
