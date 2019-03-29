4c4dfd76a4c9ab3effa50c28ccbfeaa5b3d20aeb
ProHack <bhavesh@prohack.tech>
2019-03-29 09:17:52 +0800

    finalized updater
    finalized simple updater

--- updater/update.py
+++ updater/update.py
@@ -1,26 +1,19 @@
-import urllib.request,threading,os
+import urllib.request,os
 from sys import exit
 from shutil import copyfile
 
-fileUrls,fileNames,threads=[],[],[]
+def update_dir(src):
+	dst = "../" + src
+	copyfile(src,dst)
+	os.remove(src)
+	print(f"updated {src}")
 
-def update_dir():
-	source=destination=""
-	for filename in fileNames:
-		source=filename
-		destination="../"+filename
-		if exists(filename):
-			os.delete(filename)
-		copyfile(source, destination)
-	# try determining python initials
-	get_initials=input("Enter your python command initials [EX: python3/python/py] >> ")
-	command=f"{get_initials} ../down.py"
-	Popen(command, shell=True)
-	sys.exit()
-
-def download_files(fileurl,filename):
-	#urllib.request.urlretrieve(fileurl, filename)
-	update_dir()
+def download_files(url,name):
+	try:
+		urllib.request.urlretrieve(url, name)
+	except:
+		print(f"Error downloading: {url}")
+	update_dir(name)
 
 with open("files.txt") as f:
 	lines = f.readlines()
@@ -28,12 +21,4 @@ for line in lines:
 	lineArray=line.split(" || ")
 	lineArray[0] = lineArray[0].replace("\n","")
 	lineArray[1] = lineArray[1].replace("\n","")
-	fileUrls.append(lineArray[0])
-	fileNames.append(lineArray[1])
-for x in range(0, len(fileUrls)):
-	temp_thread=threading.Thread(target=download_files,args=(fileUrls[x],fileNames[x]))
-	threads.append(temp_thread)
-for thread in threads:
-	thread.start()
-for thread in threads:
-	thread.join()
+	download_files(lineArray[0],lineArray[1])
