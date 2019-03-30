3cb0f52ac5b299bb30fc775f2e4a0c5fa0e71a48
ProHack <bhavesh@prohack.tech>
2019-03-30 21:25:22 +0800

    Update updater
    auto download file list on updater start

--- updater/update.py
+++ updater/update.py
@@ -2,12 +2,14 @@ import urllib.request,os
 from sys import exit
 from shutil import copyfile
 
+# update directories
 def update_dir(src):
 	dst = "../" + src
 	copyfile(src, dst)
 	os.remove(src)
 	print(f"updated {src}")
 
+# download files
 def download_file(url,name):
 	try:
 		urllib.request.urlretrieve(url, name)
@@ -15,6 +17,13 @@ def download_file(url,name):
 		print(f"Error downloading: {url}")
 	update_dir(name)
 
+# get the file list
+try:
+	urllib.request.urlretrieve("https://raw.githubusercontent.com/ProHackTech/pytubedown/master/updater/files.txt")
+except:
+	print(f"Error downloading: {url}")
+
+# read file list
 lines = open('files.txt').readlines()
 lineArray = [line.strip().split(' || ') for line in lines]
 for x in lineArray:
