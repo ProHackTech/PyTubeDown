003d039d52d7286e7a3878b22f5e19590961be41
ProHack <bhavesh@prohack.tech>
2019-03-31 12:03:39 +0800

    updated to v2
    added playlist support
    added single link support
    added error management
    added re-download attempt for playlist videos
    added script banner
    fixed updater update retrieval

--- down.py
+++ down.py
@@ -18,15 +18,48 @@ def replace_blank_space(_string_):
 	return _string_
 
 # download vidos
+not_downloaded = []
 def download_video(video):
-	if video=="https://www.youtube.com/None":
+	if video == "https://www.youtube.com/None":
 		print(f"{warning}Video 'None' type! Exiting..")
-		exit()
+		pass
 	else:
 		try:
 			YouTube(video).streams.first().download()
+			print(f"{success} Downloaded: {c_yellow}{video}{c_white}")
 		except:
-			pass
+			print(f"{error}[-] {video}{c_white}")
+			not_downloaded.append(video)
+
+# thread creator \(~_~)/
+def thread_ripper(video_links):
+	rippers = [threading.Thread(target=download_video, args=(video,)) for video in video_links] # create list of threads with target to download videos
+	pbar = tqdm(total=len(rippers)) # initiate progressbar
+	for ripper in rippers: # for each thread in list of threads
+		ripper.start() # start thread
+	for ripper in rippers:
+		ripper.join() # add thread to thread pool
+		pbar.update(1) # update progress bar
+
+	sleep(2)
+
+	# try again for not downloaded: one thread at a time
+	rippers = [threading.Thread(target=download_video, args=(video,)) for video in not_downloaded] # create list of threads with target to download videos
+	pbar = tqdm(total=len(rippers)) # initiate progressbar
+	for ripper in rippers: # for each thread in list of threads
+		ripper.start() # start thread
+	for ripper in rippers:
+		ripper.join() # add thread to thread pool
+		pbar.update(1) # update progress bar
+
+	# if still not downloaded, display :(
+	if len(not_downloaded) > 0:
+		print(f"{error}Unable to download following videos:")
+		for v in not_downloaded:
+			print(f"{c_yellow}{v}{c_white}")
+		print("Reattempting.. in single thread mode")
+		for v in not_downloaded:
+			download_video(v)
 
 # generate video links
 def get_vids(topic, scrl):
@@ -52,13 +85,45 @@ def get_vids(topic, scrl):
 	driver.quit()
 
 	# download videos with multiple threads
-	rippers = [threading.Thread(target=download_video, args=(video,)) for video in video_links] # create list of threads with target to download videos
-	pbar = tqdm(total=len(rippers)) # initiate progressbar
-	for ripper in rippers: # for each thread in list of threads
-		ripper.start() # start thread
-	for ripper in rippers:
-		ripper.join() # add thread to thread pool
-		pbar.update(1) # update progress bar
+	thread_ripper(video_links)
+
+def get_playlist(url):
+	# remove quoted from the link
+	url = remove_quotes(url)
+	playlist = "https://www.youtube.com/playlist?list="
+
+	# if the playlist is in watch mode
+	# because it's harder to scroll inside that tiny sidenav in YouTube
+	if "/watch?v=" in url:
+		list_link = url.split("&")[1][5:] # get list unique url
+		playlist += list_link
+	else:
+		playlist = url
+
+	# get videos link in playlist
+	video_links = []
+	driver = webdriver.Firefox()
+	driver.get(playlist)
+	sleep(5)
+	link_elems = driver.find_elements_by_class_name("ytd-playlist-video-renderer")
+	print("Gathering videos from playlist..")
+	for link_elem in link_elems:
+		watch_link = link_elem.get_attribute('href')
+		video_links.append(watch_link)
+	driver.quit()
+	# clean video_links of: None
+	print("Cleaning video list")
+	clean_list = [x for x in video_links if x is not None]
+	watch_list = []
+	for item in clean_list:
+		itemArray = item.split("&")
+		watch_list.append(itemArray[0])
+	print(f"Total Videos: {len(watch_list)}")
+	thread_ripper(watch_list)
+
+def get_link(url):
+	url = remove_quotes(url)
+	download_video(url)
 
 def read_git_version():
 	# read version file from github
@@ -89,26 +154,48 @@ parser = argparse.ArgumentParser(description="pytubedown: YouTube video download
 parser.add_argument("-t", "--topic", help="Enter topic name", type=str)
 parser.add_argument("-scrl", "--scroll", help="Enter max scroll", type=int)
 parser.add_argument("-upd", "--update", help="Update pytubedown", action="store_true")
+parser.add_argument("-pl", "--playlist", help="Download Playlist", type=str)
+parser.add_argument("-l", "--link", help="Single link download", type=str)
 args = parser.parse_args()
 
 isNetworkUp = requests.get("https://duckduckgo.com/")
 
+# ascii art
+print('''
+ ____ ___  _ _____  _     ____  _____ ____  ____  _      _     
+/  __\\  \\///__ __\\/ \\ /\\/  _ \\/  __//  _ \\/  _ \\/ \\  /|/ \\  /|
+|  \\/| \\  /   / \\  | | ||| | //|  \\  | | \\|| / \\|| |  ||| |\\ ||
+|  __/ / /    | |  | \\_/|| |_\\|  /_ | |_/|| \\_/|| |/\\||| | \\||
+\\_/   /_/     \\_/  \\____/\\____/\\____\\____/\\____/\\_/  \\|\\_/  \\|
+                                                               
+	''')
+# read version
+my_version = read_my_version()
+print(f"\n{c_green} Version: {c_white} {my_version}\n")
+
+
+
 if isNetworkUp.ok == True:
 	# update the updater that updates this which we are updating here :v
 	try:
-		urllib.request.urlretrieve("https://raw.githubusercontent.com/ProHackTech/pytubedown/master/updater/update.py", "/updater/update.py")
+		urllib.request.urlretrieve("https://raw.githubusercontent.com/ProHackTech/pytubedown/master/updater/update.py", "updater/update.py")
 	except:
-		print(f"{error}Unable to retreieve updater!{c_white}\n You can manually download it from:{c_yellow} github.com/ProHackTech/pytubedown/tree/master/updater{c_white}")
-	# read version
-	my_version = read_my_version()
-	print(f"\n{c_green} Version: {c_white} {my_version}\n")
+		print(f"{error}Unable to retreieve updater!{c_white}\n You can manually download it from:{c_yellow} github.com/ProHackTech/pytubedown/tree/master/updater{c_white}\n\n")
+	
+	# download via topic
 	if args.topic:
 		if args.scroll:
 			get_vids(args.topic, args.scroll)
 		else:
 			get_vids(args.topic, 0)
+	# update script
 	elif args.update:
 		update_me()
+	# playlist download
+	elif args.playlist:
+		get_playlist(args.playlist)
+	elif args.link:
+		get_link(args.link)
 	else:
 		print(f"{error}Please specify something{print_help}")
 else:
