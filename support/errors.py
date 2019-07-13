2240ddd881f9b337bf7420b1fa301655d8876efe
ProHack <mail@prohack.tech>
2019-07-14 06:18:49 +0800

    delete errors.py
    delete errors.py

--- support/errors.py
+++
@@ -1,6 +0,0 @@
-from support.colors import c_white, c_green, c_red, c_yellow, c_blue
-
-error = f'{c_blue}[ERROR] {c_red}'
-warning = f'{c_blue}[!] {c_yellow}'
-success = f'{c_blue}[+] {c_green}'
-print_help = f'\n{c_white}Type: -h/--help for help [Ex: script.py -h]'
